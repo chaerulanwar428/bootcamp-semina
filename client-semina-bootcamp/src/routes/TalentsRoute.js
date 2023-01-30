71495ac52de1f2a07f2985c5080a651be92b3038
chaerul anwar <chaerulanwar428@gmail.com>
2023-01-30 20:12:31 +0700

    menambahkan untuk halaman talents

---
+++ client-semina-bootcamp/src/routes/TalentsRoute.js
@@ -0,0 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
+
+import Talents from '../pages/talents';
+
+
+export function TalentsRoute() {
+  return (
+    <Routes>
+      <Route path='/' element={<Talents />} />
+    </Routes>
+  );
+}
