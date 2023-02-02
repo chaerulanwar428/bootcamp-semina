7ea1470c53b46110aecda7ce5b8291557affcaca
chaerul anwar <chaerulanwar428@gmail.com>
2023-02-03 01:33:19 +0700

    membuat page orders

--- client-semina-bootcamp/src/components/TableWithAction/index.js
+++ client-semina-bootcamp/src/components/TableWithAction/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Table } from 'react-bootstrap';
-// import Pagination from '../Pagination';
+import Pagination from '../Pagination';
 import Tbody from '../TbodyWithAction';
 import Thead from '../Thead';
 
 function TableWithAction({
-//   withoutPagination,
-//   handlePageClick,
+  withoutPagination,
+  handlePageClick,
   actionNotDisplay,
   data,
   thead,
@@ -31,11 +31,11 @@ function TableWithAction({
           customAction={customAction}
         />
       </Table>
-      {/* {!withoutPagination && data.length ? (
+      {!withoutPagination && data.length ? (
         <Pagination pages={pages} handlePageClick={handlePageClick} />
       ) : (
         ''
-      )} */}
+      )}
     </>
   );
 }
